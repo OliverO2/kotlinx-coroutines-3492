597a77f2c5b5f8846ba83a628992b67fb7db0f26
OliverO2 <oliver.o456i@gmail.com>
2022-10-26 15:21:51 +0200

    Add kotlinx-coroutines-debug dependency, making the plain test fail

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Reproducer for [kotlinx.coroutines#3492](https://github.com/Kotlin/kotlinx.coroutines/issues/3492)
 
-* This fails: `./gradlew jvmTest`
-* This works: `./gradlew -P kotestRunner=false jvmTest`
+This is an updated version including `kotlinx-coroutines-debug` dependency the as suggested
+in [this issue comment](https://github.com/Kotlin/kotlinx.coroutines/issues/3492#issuecomment-1291927426).
+
+Now the test always fails.
+
+* `./gradlew jvmTest`
+
+This is the intended behavior.
--- build.gradle.kts
+++ build.gradle.kts
@@ -24,13 +24,8 @@ kotlin {
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.4")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test-jvm:1.6.4")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:1.6.4")
                 implementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
-
-                val kotestRunner: String? by project
-                if (kotestRunner != "false") {
-                    implementation("io.kotest:kotest-runner-junit5:5.5.1")
-                }
-
                 implementation("io.projectreactor.tools:blockhound:1.0.6.RELEASE")
             }
         }
